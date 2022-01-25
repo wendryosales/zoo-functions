2427d365819c90952c6cdcca61623b023d1c7594
Wendryo S <wendryo.sales@gmail.com>
2022-01-25 17:35:49 -0300

    requisito 1 ok

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,8 +1,16 @@
+/* eslint-disable no-restricted-syntax */
+/* eslint-disable keyword-spacing */
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
-  
+function getSpeciesByIds(...ids) {
+  const array = [];
+  if (ids[0] === undefined) {
+    return array;
+  }
+  for(const id of ids) {
+    array.push(data.species.find((element) => element.id === id));
+  }
+  return array;
 }
 
 module.exports = getSpeciesByIds;
