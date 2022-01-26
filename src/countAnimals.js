ba66b8dd50c23b133e14d3ad0bab13b4f4026ca5
Wendryo S <wendryo.sales@gmail.com>
2022-01-26 16:23:45 -0300

    requisito 5 parcial ok

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,21 @@
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
-}
+const countResidents = (array) => {
+  const objeto = {};
+  array.forEach((element) => {
+    objeto[element.name] = element.residents.length;
+  });
+  return objeto;
+};
 
+function countAnimals(...animal) {
+  if (animal[0] === undefined) {
+    return countResidents(data.species);
+  }
+  if (animal[1] === undefined && animal[0] !== undefined) {
+    const findANimal = data.species.find((callback) => callback.name === animal[0]);
+    return findANimal.residents.length;
+  }
+}
+console.log(countAnimals('giraffes'));
 module.exports = countAnimals;
