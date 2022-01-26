9358b0c7a3b686bc3daad9b2e75203b5b0ce96eb
Wendryo S <wendryo.sales@gmail.com>
2022-01-26 20:37:49 -0300

    garrei no 7

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,28 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const object = { child: 0, adult: 0, senior: 0 };
+  entrants.forEach((element) => {
+    if (element.age < 18) {
+      object.child += 1;
+    }
+    if (element.age >= 18 && element.age < 50) {
+      object.adult += 1;
+    }
+    if (element.age >= 50) {
+      object.senior += 1;
+    }
+  });
+  return object;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  const { prices } = data;
+  if (entrants === undefined || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  const { child, adult, senior } = countEntrants(entrants);
+  return child * prices.child + adult * prices.adult + senior * prices.senior;
 }
 
 module.exports = { calculateEntry, countEntrants };
--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,32 @@
 const data = require('../data/zoo_data');
 
-function getAnimalMap(options) {
-  // seu código aqui
+function countRegions(local) {
+  return data.species.filter((elem) => elem.location === local).map((callback) => callback.name);
 }
 
+function countRegionsWithName(local) {
+  const findAnimal = data.species.filter((elem) => elem.location === local);
+
+  return findAnimal.forEach((callback) => callback.residents);
+}
+
+function getAnimalMap(options) {
+  if (options === undefined || options.includeNames === undefined) {
+    return {
+      NE: countRegions('NE'),
+      NW: countRegions('NW'),
+      SE: countRegions('SE'),
+      SW: countRegions('SW'),
+    };
+  }
+  if (options.includeNames === true) {
+    return {
+      NE: countRegionsWithName('NE'),
+      NW: countRegionsWithName('NW'),
+      SE: countRegionsWithName('SE'),
+      SW: countRegionsWithName('SW'),
+    };
+  }
+}
+console.log(getAnimalMap({ includeNames: true }));
 module.exports = getAnimalMap;
